b23542b87961d71fc6bda507eac1ece728c04522
stephenlau <stephenlau@grow-green.com>
2025-08-22 14:32:46 +0800

    initial commit

---
+++ asparaCamera.ts
@@ -0,0 +1,20 @@
+//% weight=100
+//% block="asparaCamera"
+//% icon="\uf06c"
+//% color="#00AAA0"
+namespace asparaCamera {
+    /**
+     * Init the asparaCamera library with serial connection
+     * @param tx Tx pin; eg: SerialPin.P2
+     * @param rx Rx pin; eg: SerialPin.P12 
+     */
+    //% blockId=asparaCamera_init block="asparaCamera init tx %tx rx %rx"
+    //% weight=101 group="Basic"
+    export function asparaCameraInit(tx: SerialPin, rx: SerialPin): void {
+        serial.redirect(tx, rx, BaudRate.BaudRate115200);
+        serial.setTxBufferSize(64)
+        serial.setRxBufferSize(64)
+        serial.readString()
+        serial.writeString('\n\n')
+    }
+}
