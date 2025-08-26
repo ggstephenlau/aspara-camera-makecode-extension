50248caa81a8aa3172bdeb449f37848acfef80e2
stephenlau <stephenlau@grow-green.com>
2025-08-26 11:34:50 +0800

    add locks for uart get result.

--- asparaCamera.ts
+++ asparaCamera.ts
@@ -3,9 +3,9 @@
 //% icon="\uf030"
 //% color="#00AAA0"
 namespace asparaCamera {
-    
-    let updateTime = input.runningTime()
     let lastResult = ""
+    let readNewdata:boolean = false;
+    let lock:boolean = false
     let newdata:boolean = false;
 
     /***********************************************************************************************************************/
@@ -25,8 +25,16 @@ namespace asparaCamera {
         serial.readString()
         serial.writeString('\n\n')
         serial.onDataReceived(serial.delimiters(Delimiters.NewLine), () => {
-            lastResult = serial.readUntil(serial.delimiters(Delimiters.NewLine));
-            newdata = true;
+            for(let i=0; i < 10; i++) {
+                if (!lock && !readNewdata) {
+                    lock = true
+                    lastResult = serial.readUntil(serial.delimiters(Delimiters.NewLine));
+                    newdata = true;
+                    lock = false;
+                    break;
+                }
+                basic.pause(1);
+            }
         });
     }
 
@@ -75,9 +83,17 @@ namespace asparaCamera {
     //% blockId=read_green_red_result block="Read Green/Red Result"
     //% group="Green Red" color="#a3b032" weight=101
     export function ReadGreenRedResult(): string {
-        let ret = lastResult
-        lastResult = ""
-        newdata = false
+        let ret = ""
+        readNewdata = true;
+        while(lock){ basic.pause(1); };
+        if (!lock) {
+            lock = true;
+            ret = lastResult
+            lastResult = ""
+            newdata = false
+            lock = false;
+        }
+        readNewdata = false;
         return ret
     }
 
@@ -112,5 +128,102 @@ namespace asparaCamera {
     //% group="Color calibration" color="#b03290" weight=102
     export function PickColor(): void {
         serial.writeLine("pick color")
+    }
+
+    /***********************************************************************************************************************/
+    /* Color tracking.                                                                                                     */
+    /***********************************************************************************************************************/
+    /**
+    * Init color tracking
+    */
+    //% blockId=color_tracking_init block="Initialize Color Tracking"
+    //% group="Color tracking" color="#dcdc14" weight=105
+    export function ColorTrackingInit(): void {
+        serial.writeLine("start color tracking")
+    }
+
+    /**
+    * Stop color tracking
+    */
+    //% blockId=stop_color_tracking block="Stop Color Tracking"
+    //% group="Color tracking" color="#dcdc14" weight=104
+    export function StopColorTracking(): void {
+        serial.writeLine("stop color tracking")
+        lastResult = ""
+        newdata = false
+    }
+
+    /**
+    * Get coordinates
+    */
+    //% blockId=get_coordinates block="Get Coordinates"
+    //% group="Color tracking" color="#dcdc14" weight=103
+    export function GetCoordinates(): void {
+        serial.writeLine("get coordinates")
+    }
+
+    /**
+    * Coordinates ready
+    */
+    //% blockId=coordinates_ready block="Coordinates ready"
+    //% group="Color tracking" color="#dcdc14" weight=102
+    export function CoordinatesReady(): boolean {
+        return newdata;
+    }
+
+    /**
+    * Read Coordinate X
+    */
+    //% blockId=read_coordinate_x block="Read Coordinate X"
+    //% group="Color tracking" color="#dcdc14" weight=101
+    export function ReadCoordinateX(): number {
+        let retnum = -1;
+
+        readNewdata = true;
+        while(lock){ basic.pause(1); };
+        if (!lock) {
+            lock = true;
+            if (lastResult.length > 0) {
+                // Parse the JSON string
+                let parsed = JSON.parse(lastResult);
+                // Access the y1 property and return it
+                if (parsed && parsed.x1 !== undefined) {
+                    retnum = parsed.x1;
+                }
+            }
+            lastResult = "";
+            newdata = false;
+            lock = false;
+        }
+        readNewdata = false;
+        return retnum;
+    }
+
+    /**
+    * Read Coordinate Y
+    */
+    //% blockId=read_coordinate_y block="Read Coordinate Y"
+    //% group="Color tracking" color="#dcdc14" weight=101
+    export function ReadCoordinateY(): number {
+        let retnum = -1;
+
+        readNewdata = true;
+        while(lock){ basic.pause(1); };
+        if (!lock) {
+            lock = true;
+            if (lastResult.length > 0) {
+                // Parse the JSON string
+                let parsed = JSON.parse(lastResult);
+                // Access the y1 property and return it
+                if (parsed && parsed.y1 !== undefined) {
+                    retnum = parsed.y1;
+                }
+            }
+            lastResult = "";
+            newdata = false;
+            lock = false;
+        }
+        readNewdata = false;
+        return retnum;
     }
 }
