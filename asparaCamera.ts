726c87e7cfe00bc2b98decc7c7bfe35741103c5d
stephenlau <stephenlau@grow-green.com>
2025-09-17 17:56:11 +0800

    line tracking

--- asparaCamera.ts
+++ asparaCamera.ts
@@ -10,8 +10,9 @@ namespace asparaCamera {
 
     export enum ModeEnum {
         Preview = 0x0,
-        GreenRed,
+        LineTracking,
         ColorTracking
+        // GreenRed,
     }
 
     export enum ColorEnum {
@@ -65,126 +66,136 @@ namespace asparaCamera {
             case ModeEnum.Preview:
                 serial.writeLine("start preview")
                 break;
-            case ModeEnum.GreenRed:
-                serial.writeLine("start green red")
+            case ModeEnum.LineTracking:
+                serial.writeLine("start line tracking")
                 break;
             case ModeEnum.ColorTracking:
                 serial.writeLine("start color tracking")
                 break;
+            // case ModeEnum.GreenRed:
+            //     serial.writeLine("start green red")
+            //     break;
         }
     }
 
     /***********************************************************************************************************************/
-    /* Green/Red                                                                                                           */
+    /* Line tracking.                                                                                                     */
     /***********************************************************************************************************************/
-    // /**
-    // * Init detect Green / Red lettuce
-    // */
-    // //% blockId=green_red_init block="Initialize Green/Red"
-    // //% group="Green Red" color="#a3b032" weight=105
-    // export function GreenRedInit(): void {
-    //     serial.writeLine("start green red")
-    // }
-
-    // /**
-    // * Start detect Green / Red lettuce
-    // */
-    // //% blockId=start_green_red block="Start Green/Red"
-    // //% group="Green Red" color="#a3b032" weight=104
-    // export function StartGreenRed(): void {
-    //     serial.writeLine("detect")
-    // }
-
     /**
-    * Stop detect Green / Red lettuce
+    * Line tracking calibrate
     */
-    //% blockId=stop_green_red block="Stop Green/Red"
-    //% group="Green Red" color="#a3b032" weight=103
-    export function StopGreenRed(): void {
-        serial.writeLine("stop green red")
+    //% blockId=line_tracking_calibrate block="Line Tracking Calibrate"
+    //% group="Line tracking" color="#dc1489" weight=104
+    export function LineTrackingCalibrate(): void {
+        serial.writeLine("calibrate")
     }
 
     /**
-    * Green / Red result ready
+    * Line tracking set color
     */
-    //% blockId=green_red_ready block="Green/Red result ready"
-    //% group="Green Red" color="#a3b032" weight=102
-    export function GreenRedResultReady(): boolean {
+    //% blockId=line_tracking_set_color block="Line Tracking Set Color %color"
+    //% group="Line tracking" color="#dc1489" weight=103
+    //% color.fieldEditor="gridpicker"
+    //% color.fieldOptions.columns=1
+    export function LineTrackingSetColor(color: ColorEnum): void {
+        let colortext = "";
+
+        switch(color) {
+            case ColorEnum.red:
+                colortext = "red";
+                break;
+            case ColorEnum.green:
+                colortext = "green";
+                break;
+            case ColorEnum.blue:
+                colortext = "blue";
+                break;
+            case ColorEnum.yellow:
+                colortext = "yellow";
+                break;
+            case ColorEnum.black:
+                colortext = "black";
+                break;
+            case ColorEnum.custom:
+                colortext = "custom";
+                break;
+        }
+        serial.writeLine("set color " + colortext)
+    }
+
+    /**
+    * Line Tracking Coordinates ready
+    */
+    //% blockId=line_tracking_coordinates_ready block="Line Tracking Coordinates ready"
+    //% group="Line tracking" color="#dc1489" weight=102
+    export function LineTrackingCoordinatesReady(): boolean {
         return newdata;
     }
 
     /**
-    * Read Green / Red lettuce result
+    * Line Tracking Read Coordinate X
     */
-    //% blockId=read_green_red_result block="Read Green/Red Result"
-    //% group="Green Red" color="#a3b032" weight=101
-    export function ReadGreenRedResult(): string {
-        let ret = ""
+    //% blockId=line_tracking_read_coordinate_x block="Line Tracking Read Coordinate X"
+    //% group="Line tracking" color="#dc1489" weight=101
+    export function LineTrackingReadCoordinateX(): number {
+        let retnum = -1;
+
         readNewdata = true;
         while(lock){ basic.pause(1); };
         if (!lock) {
             lock = true;
-            ret = lastResult
-            lastResult = ""
-            newdata = false
+            if (lastResult.length > 0) {
+                // Parse the JSON string
+                let parsed = JSON.parse(lastResult);
+                // Access the y1 property and return it
+                if (parsed && parsed.x1 !== undefined) {
+                    retnum = parsed.x1;
+                }
+            }
+            lastResult = "";
+            newdata = false;
             lock = false;
         }
         readNewdata = false;
-        return ret
+        return retnum;
     }
 
-    // /***********************************************************************************************************************/
-    // /* Color calibration                                                                                                   */
-    // /***********************************************************************************************************************/
-    // /**
-    // * Init color calibration
-    // */
-    // //% blockId=color_calibration_init block="Initialize Color Calibration"
-    // //% group="Color calibration" color="#b03290" weight=105
-    // export function ColorCalibrationInit(): void {
-    //     serial.writeLine("start color calibration")
-    // }
+    /**
+    * Line Tracking Read Coordinate Y
+    */
+    //% blockId=line_tracking_read_coordinate_y block="Line Tracking Read Coordinate Y"
+    //% group="Line tracking" color="#dc1489" weight=101
+    export function LineTrackingReadCoordinateY(): number {
+        let retnum = -1;
 
-    // /**
-    // * Stop color calibration
-    // */
-    // //% blockId=stop_color_calibration block="Stop Color Calibration"
-    // //% group="Color calibration" color="#b03290" weight=103
-    // export function StopColorCalibration(): void {
-    //     serial.writeLine("stop color calibration")
-    //     serial.writeLine("stop color calibration")
-    //     lastResult = ""
-    //     newdata = false
-    // }
-
-    // /**
-    // * Pick color
-    // */
-    // //% blockId=pick_color block="Pick Color"
-    // //% group="Color calibration" color="#b03290" weight=102
-    // export function PickColor(): void {
-    //     serial.writeLine("pick color")
-    // }
+        readNewdata = true;
+        while(lock){ basic.pause(1); };
+        if (!lock) {
+            lock = true;
+            if (lastResult.length > 0) {
+                // Parse the JSON string
+                let parsed = JSON.parse(lastResult);
+                // Access the y1 property and return it
+                if (parsed && parsed.y1 !== undefined) {
+                    retnum = parsed.y1;
+                }
+            }
+            lastResult = "";
+            newdata = false;
+            lock = false;
+        }
+        readNewdata = false;
+        return retnum;
+    }
 
     /***********************************************************************************************************************/
     /* Color tracking.                                                                                                     */
     /***********************************************************************************************************************/
-    // /**
-    // * Stop color tracking
-    // */
-    // //% blockId=stop_color_tracking block="Stop Color Tracking"
-    // //% group="Color tracking" color="#dcdc14" weight=105
-    // export function StopColorTracking(): void {
-    //     serial.writeLine("stop color tracking")
-    //     lastResult = ""
-    //     newdata = false
-    // }
-
     /**
     * Color tracking calibrate
     */
     //% blockId=color_tracking_calibrate block="Color Tracking Calibrate"
-    //% group="Color tracking" color="#dcdc14" weight=104
+    //% group="Color tracking" color="#dcdc14" weight=204
     export function ColorTrackingCalibrate(): void {
         serial.writeLine("calibrate")
     }
@@ -193,7 +204,7 @@ namespace asparaCamera {
     * Color tracking set color
     */
     //% blockId=color_tracking_set_color block="Color Tracking Set Color %color"
-    //% group="Color tracking" color="#dcdc14" weight=103
+    //% group="Color tracking" color="#dcdc14" weight=203
     //% color.fieldEditor="gridpicker"
     //% color.fieldOptions.columns=1
     export function ColorTrackingSetColor(color: ColorEnum): void {
@@ -223,20 +234,20 @@ namespace asparaCamera {
     }
 
     /**
-    * Coordinates ready
+    * Color Tracking Coordinates ready
     */
-    //% blockId=coordinates_ready block="Coordinates ready"
-    //% group="Color tracking" color="#dcdc14" weight=102
-    export function CoordinatesReady(): boolean {
+    //% blockId=color_tracking_coordinates_ready block="Color Tracking Coordinates ready"
+    //% group="Color tracking" color="#dcdc14" weight=202
+    export function ColorTrackingCoordinatesReady(): boolean {
         return newdata;
     }
 
     /**
-    * Read Coordinate X
+    * Color Tracking Read Coordinate X
     */
-    //% blockId=read_coordinate_x block="Read Coordinate X"
-    //% group="Color tracking" color="#dcdc14" weight=101
-    export function ReadCoordinateX(): number {
+    //% blockId=color_tracking_read_coordinate_x block="Color Tracking Read Coordinate X"
+    //% group="Color tracking" color="#dcdc14" weight=201
+    export function ColorTrackingReadCoordinateX(): number {
         let retnum = -1;
 
         readNewdata = true;
@@ -260,11 +271,11 @@ namespace asparaCamera {
     }
 
     /**
-    * Read Coordinate Y
+    * Color Tracking Read Coordinate Y
     */
-    //% blockId=read_coordinate_y block="Read Coordinate Y"
-    //% group="Color tracking" color="#dcdc14" weight=101
-    export function ReadCoordinateY(): number {
+    //% blockId=color_tracking_read_coordinate_y block="Color Tracking Read Coordinate Y"
+    //% group="Color tracking" color="#dcdc14" weight=201
+    export function ColorTrackingReadCoordinateY(): number {
         let retnum = -1;
 
         readNewdata = true;
@@ -287,3 +298,53 @@ namespace asparaCamera {
         return retnum;
     }
 }
+
+    /***********************************************************************************************************************/
+    /* Green/Red                                                                                                           */
+    /***********************************************************************************************************************/
+    // /**
+    // * Init detect Green / Red lettuce
+    // */
+    // //% blockId=green_red_init block="Initialize Green/Red"
+    // //% group="Green Red" color="#a3b032" weight=105
+    // export function GreenRedInit(): void {
+    //     serial.writeLine("start green red")
+    // }
+
+    // /**
+    // * Start detect Green / Red lettuce
+    // */
+    // //% blockId=start_green_red block="Start Green/Red"
+    // //% group="Green Red" color="#a3b032" weight=104
+    // export function StartGreenRed(): void {
+    //     serial.writeLine("detect")
+    // }
+
+    // /**
+    // * Green / Red result ready
+    // */
+    // //% blockId=green_red_ready block="Green/Red result ready"
+    // //% group="Green Red" color="#a3b032" weight=102
+    // export function GreenRedResultReady(): boolean {
+    //     return newdata;
+    // }
+
+    // /**
+    // * Read Green / Red lettuce result
+    // */
+    // //% blockId=read_green_red_result block="Read Green/Red Result"
+    // //% group="Green Red" color="#a3b032" weight=101
+    // export function ReadGreenRedResult(): string {
+    //     let ret = ""
+    //     readNewdata = true;
+    //     while(lock){ basic.pause(1); };
+    //     if (!lock) {
+    //         lock = true;
+    //         ret = lastResult
+    //         lastResult = ""
+    //         newdata = false
+    //         lock = false;
+    //     }
+    //     readNewdata = false;
+    //     return ret
+    // }
